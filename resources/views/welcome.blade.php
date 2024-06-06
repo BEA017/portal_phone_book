7fe44f058df15a80f0162146ca1cda086522f7a1
Evgeniy <105166308+BEA017@users.noreply.github.com>
2024-06-06 15:26:37 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
+    <link rel="stylesheet" type="text/css" href="{{asset('css/stylePortal.css') }}">
+     <script type="text/javascript" src="{{ asset('js/scriptPortal.js') }}"></script>
+        <title>Портал</title>
+ 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
